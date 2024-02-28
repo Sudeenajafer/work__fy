5703cf87c038073943530f178fa75b18dcbdad0c
sudeena <sudeenajaffer2003@gmail.com>
2024-02-28 09:45:46 +0530

    home 1

--- lib/features/user_auth/presentation/screen/home.dart
+++ lib/features/user_auth/presentation/screen/home.dart
@@ -61,94 +61,22 @@ class _HomePageState extends State<HomePage> {
             ),          ],
         ),
       ),
-      body:Container(
-
-        // children: [
-        //   ListView(
-        //
-        //     children: [
-        //       CarouselSlider(
-        //         items: [
-        //           //1st Image of Slider
-        //           Container(
-        //             margin: const EdgeInsets.all(6.0),
-        //             decoration: BoxDecoration(
-        //               borderRadius: BorderRadius.circular(8.0),
-        //               image: const DecorationImage(
-        //                 image: NetworkImage("https://c8.alamy.com/comp/PCYW3A/happy-male-driver-rides-cardriving--trip-taxi-concept-cartoon-vector-illustration-PCYW3A.jpg"),
-        //                 fit: BoxFit.cover,
-        //               ),
-        //             ),
-        //           ),
-        //
-        //
-        //
-        //           //2nd Image of Slider
-        //           Container(
-        //             margin: const EdgeInsets.all(6.0),
-        //             decoration: BoxDecoration(
-        //               borderRadius: BorderRadius.circular(8.0),
-        //               image: const DecorationImage(
-        //                 image: NetworkImage("https://c8.alamy.com/comp/PCYW3A/happy-male-driver-rides-cardriving--trip-taxi-concept-cartoon-vector-illustration-PCYW3A.jpg"),
-        //                 fit: BoxFit.cover,
-        //               ),
-        //             ),
-        //           ),
-        //
-        //           //3rd Image of Slider
-        //           Container(
-        //             margin: const EdgeInsets.all(6.0),
-        //             decoration: BoxDecoration(
-        //               borderRadius: BorderRadius.circular(8.0),
-        //               image: const DecorationImage(
-        //                 image: NetworkImage("https://th.bing.com/th/id/OIP.MJy1DHUUUFFdSPags14VNQHaFN?rs=1&pid=ImgDetMain"),
-        //                 fit: BoxFit.cover,
-        //               ),
-        //             ),
-        //           ),
-        //
-        //           //4th Image of Slider
-        //           Container(
-        //             margin: const EdgeInsets.all(6.0),
-        //             decoration: BoxDecoration(
-        //               borderRadius: BorderRadius.circular(8.0),
-        //               image: const DecorationImage(
-        //                 image: NetworkImage("https://th.bing.com/th/id/OIP.zUIe_mjeerAZ46R5I8EM-wHaFP?rs=1&pid=ImgDetMain"),
-        //                 fit: BoxFit.cover,
-        //               ),
-        //             ),
-        //
-        //           ),
-        //
-        //           //5th Image of Slider
-        //           Container(
-        //             margin: const EdgeInsets.all(6.0),
-        //             decoration: BoxDecoration(
-        //               borderRadius: BorderRadius.circular(8.0),
-        //               image: const DecorationImage(
-        //                 image: NetworkImage("https://image.freepik.com/free-vector/professional-worker-fixing-bathroom_23-2148656904.jpg"),
-        //                 fit: BoxFit.cover,
-        //               ),
-        //             ),
-        //           ),
-        //
-        //         ],
-        //
-        //         //Slider Container properties
-        //         options: CarouselOptions(
-        //           height: 180.0,
-        //           enlargeCenterPage: true,
-        //           autoPlay: true,
-        //           aspectRatio: 16 / 9,
-        //           autoPlayCurve: Curves.fastOutSlowIn,
-        //           enableInfiniteScroll: true,
-        //           autoPlayAnimationDuration: const Duration(milliseconds: 500),
-        //           viewportFraction: 0.8,
-        //         ),
-        //       ),
-        //     ],
-        //   ),
-        // ],
+      body:SingleChildScrollView(
+        child: Column(
+          children: [
+            WorkerCard(
+              image: 'assets/plumber.png',
+              name: 'John the Plumber',
+              profession: 'Plumbing Expert',
+            ),
+            WorkerCard(
+              image: 'assets/electrician.png',
+              name: 'Emma the Electrician',
+              profession: 'Electrical Specialist',
+            ),
+            // Add more WorkerCard widgets for other workers
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.search),
@@ -158,3 +86,36 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+        class WorkerCard extends StatelessWidget {
+    final String image;
+    final String name;
+    final String profession;
+
+    WorkerCard({required this.image, required this.name, required this.profession});
+
+    @override
+    Widget build(BuildContext context) {
+    return Container(
+    margin: EdgeInsets.all(16.0),
+    padding: EdgeInsets.all(16.0),
+    decoration: BoxDecoration(
+    color: Colors.grey[200],
+    borderRadius: BorderRadius.circular(10.0),
+    ),
+    child: Column(
+    children: [
+    Image.asset(image, height: 100),
+    SizedBox(height: 10),
+    Text(
+    name,
+    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+    ),
+    Text(
+    profession,
+    style: TextStyle(fontSize: 14, color: Colors.grey),
+    ),
+    ],
+    ),
+    );
+    }
+    }
