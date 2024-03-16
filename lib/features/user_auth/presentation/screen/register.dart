e4fc6420c10b2881d4f2803dff8671cabce64d46
sudeena <sudeenajaffer2003@gmail.com>
2024-03-16 15:09:51 +0530

    Update register.dart

--- lib/features/user_auth/presentation/screen/register.dart
+++ lib/features/user_auth/presentation/screen/register.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(RegisterApp());
 }
 
@@ -9,7 +12,7 @@ class RegisterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Registration Form',
+      title: 'Registration Page',
       home: RegisterPage(),
     );
   }
@@ -29,6 +32,7 @@ class _RegisterPageState extends State<RegisterPage> {
   final TextEditingController _locationController = TextEditingController();
   final TextEditingController _addressController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  String _selectedJob = 'Plumber';
 
   @override
   Widget build(BuildContext context) {
@@ -40,152 +44,169 @@ class _RegisterPageState extends State<RegisterPage> {
         padding: EdgeInsets.all(16.0),
         child: Form(
           key: _formKey,
-          child: ListView(
-            children: <Widget>[
-              TextFormField(
-                controller: _firstNameController,
-                decoration: InputDecoration(labelText: 'First Name'),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter your first name';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _lastNameController,
-                decoration: InputDecoration(labelText: 'Last Name'),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter your last name';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _ageController,
-                decoration: InputDecoration(labelText: 'Age'),
-                keyboardType: TextInputType.number,
-                validator: (value) {
-                  int age = int.tryParse(value);
-                  if (age == null || age <= 18) {
-                    return 'Age must be greater than 18';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _emailController,
-                decoration: InputDecoration(labelText: 'Email'),
-                keyboardType: TextInputType.emailAddress,
-                validator: (value) {
-                  if (value.isEmpty || !value.contains('@')) {
-                    return 'Please enter a valid email address';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                controller: _locationController,
-                decoration: InputDecoration(labelText: 'Location'),
-              ),
-              TextFormField(
-                controller: _addressController,
-                decoration: InputDecoration(labelText: 'Address'),
-              ),
-              TextFormField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: InputDecoration(labelText: 'Password'),
-                validator: (value) {
-                  if (value.isEmpty || value.length < 6) {
-                    return 'Password must be at least 6 characters long';
-                  }
-                  return null;
-                },
-              ),
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {
-                    _registerUser();
-                  }
-                },
-                child: Text('Register'),
-              ),
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                TextFormField(
+                  controller: _firstNameController,
+                  decoration: InputDecoration(labelText: 'First Name'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter your first name';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _lastNameController,
+                  decoration: InputDecoration(labelText: 'Last Name'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter your last name';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _ageController,
+                  decoration: InputDecoration(labelText: 'Age'),
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter your age';
+                    }
+                    int? age = int.tryParse(value);
+                    if (age == null || age <= 18) {
+                      return 'Age must be greater than 18';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _emailController,
+                  decoration: InputDecoration(labelText: 'Email'),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter your email';
+                    }
+                    if (!value.contains('@')) {
+                      return 'Please enter a valid email address';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _locationController,
+                  decoration: InputDecoration(labelText: 'Location'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter your location';
+                    }
+                    return null;
+                  },
+                ),
+                TextFormField(
+                  controller: _addressController,
+                  decoration: InputDecoration(labelText: 'Address'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter your address';
+                    }
+                    return null;
+                  },
+                ),
+                DropdownButtonFormField(
+                  value: _selectedJob,
+                  items: [
+                    DropdownMenuItem(child: Text('Plumber'), value: 'Plumber'),
+                    DropdownMenuItem(child: Text('Painter'), value: 'Painter'),
+                    DropdownMenuItem(child: Text('Mechanic'), value: 'Mechanic'),
+                    DropdownMenuItem(child: Text('House Keeper'), value: 'House Keeper'),
+                    DropdownMenuItem(child: Text('Tutor'), value: 'Tutor'),
+                    DropdownMenuItem(child: Text('Baby Sitter'), value: 'Baby Sitter'),
+                    DropdownMenuItem(child: Text('Electrician'), value: 'Electrician'),
+                  ],
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedJob = value!;
+                    });
+                  },
+                  decoration: InputDecoration(labelText: 'Job'),
+                ),
+                TextFormField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(labelText: 'Password'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter your password';
+                    }
+                    if (value.length < 6) {
+                      return 'Password must be at least 6 characters long';
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      _register();
+                    }
+                  },
+                  child: Text('Register'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 
-  void _registerUser() {
-    // Access Firestore and add user data
-    FirebaseFirestore.instance.collection('users').add({
-      'firstName': _firstNameController.text,
-      'lastName': _lastNameController.text,
-      'age': int.tryParse(_ageController.text),
-      'email': _emailController.text,
-      'location': _locationController.text,
-      'address': _addressController.text,
-      // You might want to consider encrypting or hashing the password before saving it to Firestore
-      'password': _passwordController.text,
-    }).then((value) {
-      // Clear text fields after successful registration
-      _firstNameController.clear();
-      _lastNameController.clear();
-      _ageController.clear();
-      _emailController.clear();
-      _locationController.clear();
-      _addressController.clear();
-      _passwordController.clear();
+  void _register() async {
+    try {
+      // Access Firestore instance
+      FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-      // Show a success message or navigate to another page
+      // Add data to Firestore
+      await firestore.collection('users').add({
+        'first_name': _firstNameController.text,
+        'last_name': _lastNameController.text,
+        'age': int.parse(_ageController.text),
+        'email': _emailController.text,
+        'location': _locationController.text,
+        'address': _addressController.text,
+        'job': _selectedJob,
+        // You can add other fields as needed
+      });
+
+      // Reset form fields
+      _formKey.currentState?.reset();
+
+      // Show success message or navigate to next screen
       showDialog(
         context: context,
-        builder: (context) => AlertDialog(
-          title: Text('Registration Successful'),
-          content: Text('User registered successfully.'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('OK'),
-            ),
-          ],
-        ),
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Success'),
+            content: Text('Registration Successful'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('OK'),
+              ),
+            ],
+          );
+        },
       );
-    }).catchError((error) {
+    } catch (e) {
       // Handle errors
-      showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: Text('Error'),
-          content: Text('An error occurred. Please try again later.'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('OK'),
-            ),
-          ],
-        ),
-      );
-    });
-  }
-
-  @override
-  void dispose() {
-    _firstNameController.dispose();
-    _lastNameController.dispose();
-    _ageController.dispose();
-    _emailController.dispose();
-    _locationController.dispose();
-    _addressController.dispose();
-    _passwordController.dispose();
-    super.dispose();
+      print('Error: $e');
+    }
   }
 }
